8c6abd532b68a5301d8f7bbadde24275a063c398
ADAMTRABELSI <100732067+ADAMTRABELSI@users.noreply.github.com>
2022-06-27 17:37:04 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import {RentalsListComponent} from "./components/tutorials-list/rentals-list.component";
+import {AddRentalComponent} from "./components/add-tutorial/add-rental.component";
+import {RentalsDetailsComponent} from "./components/tutorial-details/rentals-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'rentals', pathMatch: 'full' },
+  { path: 'rentals', component: RentalsListComponent },
+  { path: 'rentals/:id', component: RentalsDetailsComponent },
+  { path: 'add', component: AddRentalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RentalsService} from "./services/rentals.service";
+import {AddRentalComponent} from "./components/add-tutorial/add-rental.component";
+import {RentalsListComponent} from "./components/tutorials-list/rentals-list.component";
+import {RentalsDetailsComponent} from "./components/tutorial-details/rentals-details.component";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRentalComponent,
+    RentalsListComponent,
+    RentalsDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RentalsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
