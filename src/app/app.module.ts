3471b09344dffbfc388160fb6199b8337975c2f8
ImenMessaoudi <imen.messaoudi@esprit.tn>
2022-06-27 17:38:33 +0200

    first addCategorie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {CategorieService} from "./services/categorie.service";
+import { AddCategorieComponent } from './pages/add-categorie/add-categorie.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CommonModule} from "@angular/common";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCategorieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
   ],
-  providers: [],
+  providers: [CategorieService],
   bootstrap: [AppComponent]
+
+
 })
 export class AppModule { }
